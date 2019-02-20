6430f4ba87f8fdb01bdeca764c1630ecc0c12b71
AMD31 <teclado1333@gmail.com>
2019-02-20 21:22:58 +0100

    comienzo implementacion clase vuelo

--- classes/Flight.java
+++ classes/Flight.java
@@ -1,4 +1,42 @@
 package classes;
+import java.util.GregorianCalendar;
+/**
+  Flight.class
+  permite instaciar objetos de tipo vuelo.
+  @author Antonio Martinez Diaz
+*/
 public class Flight{
+
+private AirCompany aircompany;
+private Airport destinationAirport;
+private Airport originAirport;
+private GregorianCalendar dateAndTime;
+private int estimatedDuration;
+private Plane plane;
+private String code;
+
+public Flight(AirCompany aircompany, Airport destinationAirport, 
+			  Airport originAirport, GregorianCalendar dateAndTime,
+			  int estimatedDuration, Plane plane){
+			this.aircompany = aircompany;
+			this.destinationAirport = destinationAirport;
+			this.originAirport = originAirport;
+			//fecha y hora del vuelo
+			this.dateAndTime = dateAndTime;
+			this.estimatedDuration = estimatedDuration;
+			this.plane = plane;
+			this.code = generateCode();
+}	 
+
+	 private String generateCode(){
+	 	return "";
+	 }
+
+
+
+
+
+
+
 	
 }
