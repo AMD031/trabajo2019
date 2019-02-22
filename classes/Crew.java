5d96f78c2fbb80b97af229662a5dda10e46ca0fd
jinkirito <mrantoniopsn@gmail.com>
2019-02-22 08:49:28 +0100

    Subiendo inicio de la clase Crew

--- classes/Crew.java
+++ classes/Crew.java
@@ -1,4 +1,15 @@
 package classes;
+/**
+* Pilot.class
+* @author: Antonio Adamuz
+* @version: 0.0.1
+*/
 public class Crew{
-	
+	private int Amount;
+	private double Salary;
+
+	public Crew(int amount, int flytime, double salary){
+		this.Amount=amount;
+		this.Salary=salary;
+	}
 }
