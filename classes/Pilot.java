d2850f5f1c2d16be0246c6d4ac9c171d755cc96b
jinkirito <mrantoniopsn@gmail.com>
2019-02-22 08:48:42 +0100

    Subiendo inicio de la clase Pilot

--- classes/Pilot.java
+++ classes/Pilot.java
@@ -1,9 +1,17 @@
 package classes;
 /**
 * Pilot.class
-* @author
-*
+* @author: Antonio Adamuz
+* @version: 0.0.1
 */
 public class Pilot{
-	
+	private int Amount;
+	private int Flytime;
+	private double Salary;
+
+	public Pilot(int amount, int flytime, double salary){
+		this.Amount=amount;
+		this.Flytime=flytime;
+		this.Salary=salary;
+	}
 }
