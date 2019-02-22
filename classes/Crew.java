0cb9ae5210d60a0b35963748f3b2cd820da6fd09
jinkirito <mrantoniopsn@gmail.com>
2019-02-22 12:32:54 +0100

    Subiendo actualizacion 2 de la clase Crew

--- classes/Crew.java
+++ classes/Crew.java
@@ -4,10 +4,11 @@ package classes;
 * @author: Antonio Adamuz
 * @version: 0.0.1
 */
-public class Crew{
+public class Crew extends Employee{
 	private int Amount;
 	private double Salary;
-
+	//String DNI, int NEmployee, String Name, String Subname,
+ //GregorianCalendar Birthdate, String Nationaly,int quantity,String[] Lenguages
 	public Crew(int amount, int flytime, double salary){
 		this.Amount=amount;
 		this.Salary=salary;
