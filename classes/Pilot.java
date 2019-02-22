cbb94013c59b7f532267cea277fe7d9f506f3079
jinkirito <mrantoniopsn@gmail.com>
2019-02-22 12:32:25 +0100

    Subiendo actualizacion 2 de la clase Pilot

--- classes/Pilot.java
+++ classes/Pilot.java
@@ -4,14 +4,26 @@ package classes;
 * @author: Antonio Adamuz
 * @version: 0.0.1
 */
-public class Pilot{
+public class Pilot extends Employee{
+	private final static int plus=1000;
+
 	private int Amount;
 	private int Flytime;
 	private double Salary;
-
-	public Pilot(int amount, int flytime, double salary){
+	private int amountflight;
+ //String DNI, int NEmployee, String Name, String Subname,
+//GregorianCalendar Birthdate, String Nationaly,int quantity,String[] Lenguages
+	public Pilot(String DNI, int NEmployee, String Name, String Subname,
+	GregorianCalendar Birthdate, String Nationaly,int quantity,String[] Lenguages,
+	int amount, int flytime, double salary){
+		super(DNI,NEmployee,Name,Subname,Birthdate,Nationaly,quantity,Lenguages);
 		this.Amount=amount;
 		this.Flytime=flytime;
 		this.Salary=salary;
 	}
+ public double calculateSalary(){
+	 return 1;
+ }
+
+
 }
