784fd39d74f468e865d80b45a325d178a3b70d90
AMD31 <teclado1333@gmail.com>
2019-02-24 18:34:23 +0100

    Actualizanco clase Flight

--- classes/Flight.java
+++ classes/Flight.java
@@ -1,13 +1,13 @@
 package classes;
-
+import java.util.ArrayList;
 import java.util.GregorianCalendar;
+import java.util.Iterator;
 /**
   Flight.class
   permite instaciar objetos de tipo Flight.
   contiene informacion sobre: la compañia, aeropuerto destino y origen,
   fecha del vuelo en un objeto del tipo GregorianCalendar, tiempo estimado del vuelo,
   avion y codiogo del vuelo.
-
   @author Antonio Martinez Diaz
 */
 public class Flight{
@@ -19,10 +19,11 @@ private GregorianCalendar dateAndTime;
 private int estimatedDuration;
 private Plane plane;
 private String code;
+private ArrayList<Seat> SeatsFlight;
 
 public Flight(AirCompany aircompany, Airport destinationAirport, 
 			  Airport originAirport, GregorianCalendar dateAndTime,
-			  int estimatedDuration, Plane plane){
+			  int estimatedDuration, Plane plane)throws Exception{
 			this.aircompany = aircompany;
 			this.destinationAirport = destinationAirport;
 			this.originAirport = originAirport;
@@ -31,6 +32,11 @@ public Flight(AirCompany aircompany, Airport destinationAirport,
 			this.estimatedDuration = estimatedDuration;
 			this.plane = plane;
 			this.code = generateCode();
+			this.initializeSeats(plane);
+			/*for (Seat a : SeatsFlight ) {
+				System.out.println(a);
+			}*/
+
 }	 
 
 	 private String generateCode(){
@@ -43,19 +49,52 @@ public Flight(AirCompany aircompany, Airport destinationAirport,
 			tmpcode.append(dateAndTime.HOUR);
 			tmpcode.append(dateAndTime.MINUTE);
 			tmpcode.append("SVQ");//destinationAirport.getAcronym();
+
+
 			 
 		 return tmpcode.toString();
 	 }
+
+
 
 	 public String getCode(){
 	 	return this.code;
 	 }
 
+	 public Plane getPlane(){
+	 	return this.plane;
+	 } 
 
+	 public void setPlane(Plane p)throws Exception{
+	 	if(p!=null){
+	 		int oldPlaneTam = plane.getRows()*plane.getColums();
+	 		int newPlaneTam = p.getRows()*p.getColums();
+	 		if(oldPlaneTam == newPlaneTam ){
+	 			 this.plane = p;
+	 		}else{
+	 			throw new Exception("No se puede transferir todas las reservas al nuevo avion.");
+	 		}
+	 	}
+	 }
 
-
-
-
-
-	
+	 private void initializeSeats(Plane plane)throws NullPointerException{
+		 	SeatsFlight = new ArrayList<Seat>();
+		 	int Nvip = plane.getNvip();//numero de filas vip
+		 	boolean vip = false;
+		 	boolean re = true;
+		 	char CharC = 'A';//letra de la columna
+		 	int Nrow = 0;
+		 	for (int i =0;i<plane.getRows()*plane.getColums();i++ ) {
+	 			Nrow++;
+	 			if(Nrow>plane.getRows()){
+	 				Nrow=1;
+	 				CharC = (char)((int)CharC+1);
+	 			}
+	 		   if(Nrow<Nvip){
+	 				vip = true;
+	 		   }
+			   SeatsFlight.add(new Seat(vip,Nrow,CharC));
+	 		vip = false;
+ 		}
+	 }
 }
