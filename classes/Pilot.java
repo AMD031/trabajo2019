f4bc92006845c911f6456ea2bf6009a9130660b7
jinkirito <mrantoniopsn@gmail.com>
2019-02-26 11:22:53 +0100

    Subiendo actualizacion 3  de la clase Pilot con el toString y arreglo de errores(compila)

--- classes/Pilot.java
+++ classes/Pilot.java
@@ -7,25 +7,31 @@ import java.util.ArrayList;
 * @version: 0.0.1
 */
 public class Pilot extends Employee{
-	private final static int plus=5000;
+	private final static double plus=5000;
 	private final static double Salary=100000;
-	private int Amount;
+	private int assignedFlight;
 	private int Flytime;
 
 	private int amountflight;
  //String DNI, int NEmployee, String Name, String Subname,
 //GregorianCalendar Birthdate, String Nationaly,int quantity,String[] Lenguages
 	public Pilot(String DNI,String Name, String Subname,
-	GregorianCalendar Birthdate, String Nationaly,int quantity,
-	ArrayList<String> Lenguages,
-	int amount, int flytime)throws Exception{
+	GregorianCalendar Birthdate, String Nationaly,
+	ArrayList<String> Lenguages, int flytime)throws Exception{
 		super(DNI,Name,Subname,Birthdate,Nationaly,Lenguages);
-		this.Amount=amount;
 		this.Flytime=flytime;
 	}
- public double calculateSalary(amountflight, plus){
-	 this.Salary= this.Salary + this.amountflight * this.plus;
+ public double calculateSalary(){
+	 double total;
+	 total=this.assignedFlight * this.plus;
+	 total=this.Salary + total;
+	 return total;
  }
 
+ @Override
+	 public String toString(){
+		 return super.toString()+" Total Salary: "+calculateSalary()+"\n Assigned Flight: "
+		 +this.assignedFlight+"\n Fly time: "+this.Flytime;
+	 }
 
 }
