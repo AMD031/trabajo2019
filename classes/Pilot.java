30e7c92dbe17e53dfd73fba317012d823ecac2fe
jinkirito <mrantoniopsn@gmail.com>
2019-02-26 14:52:24 +0100

    Subiendo comentarios y correciones de la clase Pilot

--- classes/Pilot.java
+++ classes/Pilot.java
@@ -3,6 +3,8 @@ import java.util.GregorianCalendar;
 import java.util.ArrayList;
 /**
 * Pilot.class
+* Descripcion: Clase Pilot que hereda los metodos de la clase Employee y tiene
+* metodos propios y utiliza el constructor abstracto de la clase padre.
 * @author: Antonio Adamuz
 * @version: 0.0.1
 */
@@ -12,9 +14,7 @@ public class Pilot extends Employee{
 	private int assignedFlight;
 	private int Flytime;
 
-	private int amountflight;
- //String DNI, int NEmployee, String Name, String Subname,
-//GregorianCalendar Birthdate, String Nationaly,int quantity,String[] Lenguages
+
 	public Pilot(String DNI,String Name, String Subname,
 	GregorianCalendar Birthdate, String Nationaly,
 	ArrayList<String> Lenguages, int flytime)throws Exception{
@@ -26,6 +26,24 @@ public class Pilot extends Employee{
 	 total=this.assignedFlight * this.plus;
 	 total=this.Salary + total;
 	 return total;
+ }
+
+ //setter
+ public void setAssignedFlight(int flight){
+	 this.assignedFlight=flight;
+ }
+
+ public void setFlytime(int time){
+	 this.Flytime=time;
+ }
+
+ //getter
+ public double getAssignedFlight(){
+	 return this.assignedFlight;
+ }
+
+ public double getFlytime(){
+	 return this.Flytime;
  }
 
  @Override
