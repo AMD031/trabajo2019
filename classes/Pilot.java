b729a0deae93d70176aa49c154dc2f9ca93fcf35
jinkirito <mrantoniopsn@gmail.com>
2019-02-26 09:06:45 +0100

    Subiendo actualizacion 3  de la clase Pilot

--- classes/Pilot.java
+++ classes/Pilot.java
@@ -1,28 +1,30 @@
 package classes;
+import java.util.GregorianCalendar;
+import java.util.ArrayList;
 /**
 * Pilot.class
 * @author: Antonio Adamuz
 * @version: 0.0.1
 */
 public class Pilot extends Employee{
-	private final static int plus=1000;
-
+	private final static int plus=5000;
+	private final static double Salary=100000;
 	private int Amount;
 	private int Flytime;
-	private double Salary;
+
 	private int amountflight;
  //String DNI, int NEmployee, String Name, String Subname,
 //GregorianCalendar Birthdate, String Nationaly,int quantity,String[] Lenguages
-	public Pilot(String DNI, int NEmployee, String Name, String Subname,
-	GregorianCalendar Birthdate, String Nationaly,int quantity,String[] Lenguages,
-	int amount, int flytime, double salary){
-		super(DNI,NEmployee,Name,Subname,Birthdate,Nationaly,quantity,Lenguages);
+	public Pilot(String DNI,String Name, String Subname,
+	GregorianCalendar Birthdate, String Nationaly,int quantity,
+	ArrayList<String> Lenguages,
+	int amount, int flytime)throws Exception{
+		super(DNI,Name,Subname,Birthdate,Nationaly,Lenguages);
 		this.Amount=amount;
 		this.Flytime=flytime;
-		this.Salary=salary;
 	}
- public double calculateSalary(){
-	 return 1;
+ public double calculateSalary(amountflight, plus){
+	 this.Salary= this.Salary + this.amountflight * this.plus;
  }
 
 
