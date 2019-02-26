20e66471cecffd211cd05620dc1b8024d1756f68
jinkirito <mrantoniopsn@gmail.com>
2019-02-26 13:05:45 +0100

    Subiendo actualizacion 3  de la clase Crew con el toString y arreglo de errores(compila)

--- classes/Crew.java
+++ classes/Crew.java
@@ -7,18 +7,26 @@ import java.util.ArrayList;
 * @version: 0.0.1
 */
 public class Crew extends Employee{
-	private int Amount;
-	private double Salary;
+	private final static double Salary=40000;
+	private final static double plus=1000;
+	private int assignedFlight;
 
 	public Crew(String DNI,String Name, String Subname,
 	 GregorianCalendar Birthdate,
-	 String Nationaly,ArrayList<String> Lenguages, int Amount, double Salary)throws Exception{
+	 String Nationaly,ArrayList<String> Lenguages)throws Exception{
 		super(DNI,Name,Subname,Birthdate,Nationaly,Lenguages);
-		this.Amount=Amount;
-		this.Salary=Salary;
 	}
 
 	public double calculateSalary(){
-		return this.Salary;
+		double total;
+		total = this.plus * this.assignedFlight;
+		total=total + this.Salary;
+		return total;
 	}
+
+	@Override
+ 	 public String toString(){
+ 		 return super.toString()+" Total Salary: "+calculateSalary()+"\n Assigned Flight: "
+ 		 +this.assignedFlight;
+ 	 }
 }
