3b98d85fc7f5eabaa100867c25de5ac53540d92c
jinkirito <mrantoniopsn@gmail.com>
2019-02-27 13:38:07 +0100

    peuqña modificacion en el toString de la clase Person

--- classes/Person.java
+++ classes/Person.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 	Person.class
 	clase que permite instaciar objetos de tipo persona con
 	los datos dni, nombre, apellido y nacionalidad de tipo String
-	y una fecha de tipo gregorian calendar. 
+	y una fecha de tipo gregorian calendar.
 	@author Antonio Martinez Diaz
 */
 public class Person{
@@ -19,12 +19,12 @@ public class Person{
 	protected boolean cangetjob;
 
 	protected final static String DNIDEF = "30151277W";
-	protected final static String NAMEDEF ="";		
-	protected final static String SUBNAMEDEF =""; 
+	protected final static String NAMEDEF ="";
+	protected final static String SUBNAMEDEF ="";
 	protected final static String NATINALITYDEF="";
 	protected final static GregorianCalendar BRITHDATEDEF= new GregorianCalendar(1999,2,3);
-	
-	protected Person(String dni, String name, String subName, 
+
+	protected Person(String dni, String name, String subName,
 		             GregorianCalendar birthDate,  String Natinality)throws Exception{
 		this.setDni(dni);
 		this.name = new String(name);
@@ -40,7 +40,7 @@ public class Person{
 
 
     /**
-	* El metodo comprueba si el formato y letra son validos  
+	* El metodo comprueba si el formato y letra son validos
 	* @author Antonio Martinez Diaz
 	* @param String dni: es el dni a comprobar
 	* @return devuelve true si el dni es correcto.
@@ -49,17 +49,17 @@ public class Person{
 	    boolean valid = false;
 		String patternDni = "\\d{8}[A-HJ-NP-TV-Za-hj-np-tv-z]$";
 		String patternDni2 = "^[X-Z]\\d{7}[A-HJ-NP-TV-Za-hj-np-tv-z]$";
-	
+
 	    if(Pattern.matches(patternDni, dni)==true || Pattern.matches(patternDni2, dni)==true){
 			char lastLetter = Character.toUpperCase(dni.charAt(dni.length()-1));
 			char firstLetter = Character.toUpperCase(dni.charAt(0));
-			
+
 			if(firstLetter == 'X'  ){
 				dni = dni.replaceFirst("X", "0");
 				dni = dni.replaceFirst("x", "0");
 			}else if(firstLetter == 'Y' ){
 				dni = dni.replaceFirst("Y", "1");
-				dni = dni.replaceFirst("y", "1");	
+				dni = dni.replaceFirst("y", "1");
 			}else if(firstLetter == 'Z' ){
 				dni = dni.replaceFirst("Z", "2");
 				dni = dni.replaceFirst("z", "2");
@@ -68,7 +68,7 @@ public class Person{
 			String numbersString = " ";
 			int numbersInt = 0;
 			int rest =0;
-			
+
 			char[] letters = {'T', 'R', 'W','A','G','M','Y','F','P','D','X','B',
 							 'N','J','Z','S','Q','V','H','L','C','K','E'};
 
@@ -83,24 +83,24 @@ public class Person{
 		return valid;
 
 	}
-	
+
 	/**
 		Metodo que comprueba si es mayor de la edad indicada.
 		@param date fecha de nacimiento de la persona
 		@param age edad que se quiere verificar con la fecha.
 		@return devuelve true si es mayor de la edad indicada
-	*/	
+	*/
 	public boolean checkAge(GregorianCalendar date,int age){
 		GregorianCalendar rightNow = new GregorianCalendar();
 		rightNow.add(Calendar.YEAR,-age);
 		return date.before(rightNow);
-	}	
+	}
  	//setter
 	/**
 		Cambia el valor del dni en la variable del objeto si el dni es valido
 		@param dni valor de tipo String que contedra el dni.
 		@exception Exception si el dni no es valido.
-		
+
 	*/
 	public void setDni(String dni)throws Exception{
 		if(checkDni(dni)){
@@ -156,14 +156,14 @@ public class Person{
 
 	@Override
 	public String toString(){
-		return "Name: "+this.name+"\n Subname: "+this.subName+" Dni: "+this.dni+
-		" Natinality: "+this.Natinality+" Birth date: "+this.getBirthDate();
+		return "Name: "+this.name+"\n Subname: "+this.subName+"\n Dni: "+this.dni+
+		"\n Natinality: "+this.Natinality+"\n Birth date: "+this.getBirthDate();
 	}
 
 	@Override
 	public boolean equals(Object obj){
 		boolean match = false;
-		if(obj instanceof Person){	
+		if(obj instanceof Person){
 			Person tmp = (Person)obj;
 			if(this.getDni().equals(tmp.getDni())){
 			   match =true;
