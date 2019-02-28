ec24bbb15f30e59d8a01de3f740cf7955c49e425
AMD31 <teclado1333@gmail.com>
2019-02-28 20:25:44 +0100

    modificacion del interface IAirCompany

--- interfaces/IAirCompany.java
+++ interfaces/IAirCompany.java
@@ -3,25 +3,25 @@ import java.util.ArrayList;
 import classes.*;
 
 public interface IAirCompany{
-	public boolean hireEmployee(Employee e);
+	public boolean hireEmployee(Employee e)throws Exception;
 	public boolean fireEmployee(String dni, String NEmployee);
-	public ArrayList<Employee>listEmployee();
-	public boolean searchEMployee(String name,String dni, String NEmployee);
+	public ArrayList<Employee>listEmployees();
+	public Employee searchEMployee(String name,String dni, int NEmployee);
 	public double totalSalary();
-	public boolean addPlane(Plane p);
-	public ArrayList<Plane>listPlane();
+	public boolean addPlane(Plane p)throws Exception;
+	public ArrayList<Plane>listPlanes();
 	public boolean removePlane(String idPlane);
 	public Plane searchPlane(String idPlane);
 	public boolean addFlight(Flight f);
 	public ArrayList<Flight> listFlight();
-	public boolean serachFlight(String destinationAirpor
+	public Flight serachFlight(String destinationAirpor
 	, String originAirport);
 	public boolean removeFlight(String code);
-	public boolean buyTicket(String codeFlight);
-	public boolean removeTicket();
+	public boolean buyTicket(Ticket t);
+	public boolean removeTicket(String dni, String id);
 	public Ticket searchTicket(String dni, String id);
-	public boolean addClient(Client client);
-	public ArrayList<Client> listClient();
+	public boolean addClient(Client client)throws Exception;
+	public ArrayList<Client> listClients();
 	public Client searchClient(String dni);
 	public boolean removeClient(String dni);	
 }
